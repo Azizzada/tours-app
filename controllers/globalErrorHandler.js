78bf2a17e9e081e405e3ad58c0c0650146ef63c2
Enayatullah Azizzada <95454760+Azizzada@users.noreply.github.com>
2023-05-26 16:19:59 -0400

    error handling

--- controllers/globalErrorHandler.js
+++ controllers/globalErrorHandler.js
@@ -1,3 +1,61 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorFromMongoDB = (error) => {
+  // using the content of error and creating our own custum error
+  const message = `Invalid ${error.path}: ${error.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldMongoDB = (error) => {
+  // making this error message userfriendly "E11000 duplicate key error collection: tours-app.tours index: name_1 dup key: { name: \"The Snow Adventurer\" }""
+  const message = `Duplicate field value: ${error.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorMongoDB = (error) => {
+  const errors = Object.values(error.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+sendErrorDevelopment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+sendErrorProductionOperationalTrue = (err, res) => {
+  // Operational, trusted error: send mesage to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // programming or other unknows error: we dont leak error details to client
+  } else {
+    // 1) Log error
+    console.log('ERROR 💥', err);
+    // 1) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+sendErrorProductionOperationalFalse = (err, res) => {
+  // Operational, trusted error: send mesage to client
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+  // programming or other unknows error: we dont leak error details to clien
+};
+
 // this is a error middleware that runs last
 module.exports = (err, req, res, next) => {
   // statusCode => status(404 or 500 or any number)
@@ -7,28 +65,26 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error'; // means if there is no status then the status is error
 
   if (process.env.NODE_ENV === 'development') {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
+    sendErrorDevelopment(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    // Operational, trusted error: send mesage to client
-    if (err.isOperational) {
-      res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message,
-      });
-      // programming or other unknows error: we dont leak error details to client
-    } else {
-      // 1) Log error
-      console.log('ERROR 💥', err);
-      // 1) Send generic message
-      res.status(500).json({
-        status: 'error',
-        message: 'Something went wrong!',
-      });
+    // in order to handle mongoose errors which are not user friedly we will need to change the structure of those error so
+    // that is why we are creating a variable from the err and we are going to make changes to it. they come from validator from mongoose and mongo
+    // i first tried to copy using destructuring then this method. it didnt work so i tried this method
+    let error = JSON.parse(JSON.stringify(err));
+    if (error.name === 'CastError') {
+      error = handleCastErrorFromMongoDB(error);
+      return sendErrorProductionOperationalFalse(error, res);
     }
+    if (error.code === 11000) {
+      // making this error message userfriendly "E11000 duplicate key error collection: tours-app.tours index: name_1 dup key: { name: \"The Snow Adventurer\" }""
+      error = handleDuplicateFieldMongoDB(error);
+      return sendErrorProductionOperationalFalse(error, res);
+    }
+    if (error.name === 'ValidationError') {
+      error = handleValidationErrorMongoDB(error);
+      return sendErrorProductionOperationalFalse(error, res);
+    }
+
+    sendErrorProductionOperationalTrue(err, res); //this will handle isOperational and other errors EXECPT MONGOOSE ERROR which is handled above
   }
 };
