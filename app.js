dfe127aa379ebccd4b625515c2d848b78fb85ba0
Enayatullah Azizzada <95454760+Azizzada@users.noreply.github.com>
2023-06-06 20:37:49 -0400

    Improved stripe implementation

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
-
+const bookingController = require('./controllers/bookingController');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/globalErrorHandler');
 
@@ -58,6 +58,15 @@ const limiter = rateLimit({
 
 // setting limit to all the routes starting with /api
 app.use('/api', limiter);
+
+// this routes is defined in STRIPE
+// reason for defining this route here: when we receive the body from stripe, it needs to ready the body
+// in raw form, as STRING and not as JSON. so therefore we need to define it before the body is converted into JSON
+app.post(
+  '/web-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 // Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
