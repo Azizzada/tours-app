9b4c5b8bdd5354fb6e52fa4768197f1dc0551a53
Enayatullah Azizzada <95454760+Azizzada@users.noreply.github.com>
2023-06-06 22:19:36 -0400

    improving stripe3

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,10 +60,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.object.amount_total / 100;
-  await Booking.create({ tour, user, price });
+  console.log(session);
+  // const tour = session.client_reference_id;
+  // const user = (await User.findOne({ email: session.customer_email })).id;
+  // const price = session.object.amount_total / 100;
+  // await Booking.create({ tour, user, price });
 };
 
 exports.webhookCheckout = (req, res, next) => {
